6f422878270e91eeb7137cc547ad77385bbcf7ba
Ch'ng Seng Hong <amoschng040719@gmail.com>
2026-01-19 17:20:03 +0800

    Update Code

--- src/main/java/com/soulspace/dao/LearningLessonDAO.java
+++ src/main/java/com/soulspace/dao/LearningLessonDAO.java
@@ -1,12 +1,11 @@
 package com.soulspace.dao;
 
-import com.soulspace.model.LearningLesson;
 import java.util.List;
+
+import com.soulspace.model.LearningLesson;
 
 public interface LearningLessonDAO {
     List<LearningLesson> findByCourseId(Long courseId);
     void save(LearningLesson lesson);
-    
-    // --- ADD THIS METHOD ---
     LearningLesson findById(Long id);
 }
